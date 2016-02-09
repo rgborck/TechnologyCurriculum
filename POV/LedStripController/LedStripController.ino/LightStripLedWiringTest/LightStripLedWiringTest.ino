544484d0c6e514af18af2e96e1210db42270fe68
rgborck <rgborck@yahoo.com>
2016-02-09 00:52:43 -0500

    Led strip test and WIP message code

---
+++ POV/LedStripController/LedStripController.ino/LightStripLedWiringTest/LightStripLedWiringTest.ino
@@ -0,0 +1,22 @@
+int pins[] = {2,3,4,5,6,7,8,9};
+
+void setup() {
+  for(int i=0; i<sizeof(pins)/sizeof(int); i++){
+    pinMode(pins[i], OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  for(int i=0; i<sizeof(pins)/sizeof(int);i++) { 
+    AllOff(); 
+    digitalWrite(pins[i], HIGH); 
+    delay(1000); 
+  }
+}
+
+void AllOff(){
+  for (int i=0; i<sizeof(pins)/sizeof(int);i++) {
+    digitalWrite(pins[i], LOW); 
+  }
+}
